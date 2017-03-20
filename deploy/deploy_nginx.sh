b85329ebc4d0d64e2707bd6347b85f09a229aa37
Vladislav Tomenko <vladislavtomenko@gmail.com>
2017-03-20 19:47:28 +0700

    feat: include SSL secrets

--- deploy/deploy_nginx.sh
+++ deploy/deploy_nginx.sh
@@ -20,6 +20,7 @@ apt-get install gettext-base -y
 
 # update kubernetes Deployment file
 envsubst < deploy/kubernetes/nginx_deployment.yml.template > deploy/kubernetes/nginx_deployment.yml
+envsubst < deploy/kubernetes/secrets.yml.template > deploy/kubernetes/secrets.yml
 
 cat deploy/kubernetes/nginx_deployment.yml
 
@@ -28,6 +29,7 @@ deployment_flag=$(GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl get dep
 
 if [[ -z "$deployment_flag" ]]; then
   echo "Create new deployment and service for ${APPLICATION_NAME}"
+  GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl create -f deploy/kubernetes/secrets.yml
   GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl create -f deploy/kubernetes/nginx_deployment.yml
   GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl expose deployment ${APPLICATION_NAME} --name=${APPLICATION_NAME} --port=80,443 --type="LoadBalancer"
 else
